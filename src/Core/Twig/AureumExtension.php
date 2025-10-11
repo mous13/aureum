6a23f37a00c8168a77e2a8cce116c396761a051c
Oscar Ellis <oscarx1be@gmail.com>
2025-10-11 18:46:13 +0000

    override verification email

--- src/Core/Twig/AureumExtension.php
+++ src/Core/Twig/AureumExtension.php
@@ -5,11 +5,13 @@ namespace Citadel\Aureum\Core\Twig;
 use Citadel\Aureum\Core\MenuBuilder\AureumMenuManager;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
+use Citadel\Aureum\Core\Repository\EmployeeRepository;
 
 class AureumExtension extends AbstractExtension
 {
     public function __construct(
-        private readonly AureumMenuManager $aureumMenuManager
+        private readonly AureumMenuManager $aureumMenuManager,
+        private readonly EmployeeRepository $employeeRepository,
     ){
     }
 
@@ -17,7 +19,15 @@ class AureumExtension extends AbstractExtension
     {
         return [
             new TwigFunction('aureum_menu', $this->aureumMenuManager->getMenu(...)),
+            new TwigFunction('employee', [$this, 'getEmployee'])
         ];
+    }
+
+    public function getEmployee(User $user): User
+    {
+        $employee = $this->employeeRepository->findOneBy(['user' => $user]);
+        
+        return $employee;
     }
 
 }
