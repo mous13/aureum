c50f455bacd4daf5e8bff7f361769be39c642e48
Oscar Ellis <oscarx1be@gmail.com>
2025-10-22 18:04:49 +0100

    extend abstractlogrepository

--- src/Core/Repository/FineLogRepository.php
+++ src/Core/Repository/FineLogRepository.php
@@ -2,38 +2,26 @@
 
 namespace Citadel\Aureum\Core\Repository;
 
-use Citadel\Aureum\Core\Entity\Hotel;
 use Citadel\Aureum\Core\Entity\Fine;
 use Citadel\Aureum\Core\Entity\FineLog;
-use Forumify\Core\Repository\AbstractRepository;
 
 /**
- * @extends AbstractRepository<FineLog>
+ * @extends AbstractLogRepository<FineLog>
  */
-class FineLogRepository extends AbstractRepository
+class FineLogRepository extends AbstractLogRepository
 {
     public static function getEntityClass(): string
     {
         return FineLog::class;
     }
 
-    public function findByFine(Fine $fine): array
+    protected function getLogEntityReference(): string
     {
-        return $this->createQueryBuilder('f')
-            ->where('f.fine = :fine')
-            ->setParameter('fine', $fine)
-            ->orderBy('f.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return 'fine';
     }
 
-    public function findRecentByHotel(Hotel $hotel): array
+    public function findByFine(Fine $fine): array
     {
-        return $this->createQueryBuilder('f')
-            ->where('f.hotel = :hotel')
-            ->setParameter('hotel', $hotel)
-            ->orderBy('f.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return $this->findByEntity($fine);
     }
 }
--- src/Core/Repository/LostPropertyLogRepository.php
+++ src/Core/Repository/LostPropertyLogRepository.php
@@ -2,38 +2,26 @@
 
 namespace Citadel\Aureum\Core\Repository;
 
-use Citadel\Aureum\Core\Entity\Hotel;
 use Citadel\Aureum\Core\Entity\LostProperty;
 use Citadel\Aureum\Core\Entity\LostPropertyLog;
-use Forumify\Core\Repository\AbstractRepository;
-
 /**
- * @extends AbstractRepository<LostPropertyLog>
+ * @extends AbstractLogRepository<LostPropertyLog>
  */
-class LostPropertyLogRepository extends AbstractRepository
+class LostPropertyLogRepository extends AbstractLogRepository
 {
+
     public static function getEntityClass(): string
     {
         return LostPropertyLog::class;
     }
 
-    public function findByLostProperty(LostProperty $lostProperty): array
+    protected function getLogEntityReference(): string
     {
-        return $this->createQueryBuilder('l')
-            ->where('l.lostProperty = :property')
-            ->setParameter('property', $lostProperty)
-            ->orderBy('l.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return 'lostProperty';
     }
 
-    public function findRecentByHotel(Hotel $hotel): array
+    public function findByLostProperty(LostProperty $lostProperty): array
     {
-        return $this->createQueryBuilder('l')
-            ->where('l.hotel = :hotel')
-            ->setParameter('hotel', $hotel)
-            ->orderBy('l.createdAt', 'DESC')
-            ->getQuery()
-            ->getResult();
+        return $this->findByEntity($lostProperty);
     }
 }
