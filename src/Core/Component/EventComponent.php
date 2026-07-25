ba4e97168f1266b2458dbb574b7b1df3875174b3
Oscar Ellis <oscarx1be@gmail.com>
2026-07-25 02:39:39 +0100

    role restrict editor mode

--- src/Core/Component/EventComponent.php
+++ src/Core/Component/EventComponent.php
@@ -5,18 +5,21 @@ declare(strict_types=1);
 namespace Citadel\Aureum\Core\Component;
 
 use Citadel\Aureum\Core\Entity\Enum\EventType;
+use Citadel\Aureum\Core\Entity\Enum\EmployeeRole;
 use Citadel\Aureum\Core\Entity\Event;
 use Citadel\Aureum\Core\Form\EventFormType;
 use Citadel\Aureum\Core\Repository\EventRepository;
 use Citadel\Aureum\Core\Service\AureumService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
+
 
 #[AsLiveComponent('Aureum\\EventCalendar', '@CitadelAureum/core/components/event_calendar.html.twig')]
 class EventComponent extends AbstractController
@@ -33,6 +36,9 @@ class EventComponent extends AbstractController
     #[LiveProp]
     public ?string $createDay = null;
 
+    #[LiveProp]
+    public ?int $editEventId = null;
+
     public function __construct(
         private readonly AureumService $aureumService,
         private readonly EventRepository $eventRepository,
@@ -47,8 +53,9 @@ class EventComponent extends AbstractController
     #[LiveAction]
     public function toggleEditor(): void
     {
+        $this->denyUnlessAllowed();
         $this->editorMode = !$this->editorMode;
-        $this->createDay = null;
+        $this->closeModal();
     }
 
     #[LiveAction]
@@ -57,51 +64,101 @@ class EventComponent extends AbstractController
         $this->week = $this->getWeekStart()
             ->modify(($direction * 7) . ' days')
             ->format('Y-m-d');
-        $this->createDay = null;
+        $this->closeModal();
     }
 
     #[LiveAction]
     public function openCreate(#[LiveArg] string $day): void
     {
+        $this->denyUnlessAllowed();
+        $this->editEventId = null;
         $this->createDay = $day;
         $this->resetForm();
     }
 
     #[LiveAction]
-    public function closeCreate():void
+    public function openEdit(#[LiveArg] int $eventId): void
+    {
+        $this->denyUnlessAllowed();
+        $this->createDay = null;
+        $this->editEventId = $eventId;
+        $this->resetForm();
+    }
+
+    #[LiveAction]
+    public function closeModal(): void
     {
         $this->createDay = null;
+        $this->editEventId = null;
         $this->resetForm();
     }
 
     #[LiveAction]
     public function save(): void
     {
+        $this->denyUnlessAllowed();
         $this->submitForm();
 
+        /** @var Event $event */
         $event = $this->getForm()->getData();
-        $event->setHotel($this->aureumService->getHotel());
-        $event->setCreatedBy($this->aureumService->getEmployee());
+
+        if ($this->editEventId === null) {
+            $event->setHotel($this->aureumService->getHotel());
+            $event->setCreatedBy($this->aureumService->getEmployee());
+        }
 
         $this->eventRepository->save($event);
+        $this->closeModal();
+    }
 
-        $this->closeCreate();
+    #[LiveAction]
+    public function delete(): void
+    {
+        $this->denyUnlessAllowed();
+        $event = $this->findEditEvent();
+        if ($event !== null) {
+            $this->eventRepository->remove($event);
+        }
+
+        $this->closeModal();
+    }
+
+    public function getEditEvent(): ?Event
+    {
+        return $this->findEditEvent();
     }
 
     protected function instantiateForm(): FormInterface
     {
-        $event = new Event();
+        $event = $this->findEditEvent();
 
-        $day = $this->createDay !== null
-            ? \DateTimeImmutable::createFromFormat('!Y-m-d', $this->createDay)
-            : false;
-        $event->setEventDate(($day ?: $this->getWeekStart())->setTime(18, 0));
+        if ($event === null) {
+            $event = new Event();
+            $day = $this->createDay !== null
+                ? \DateTimeImmutable::createFromFormat('!Y-m-d', $this->createDay)
+                : false;
+            $event->setEventDate(($day ?: $this->getWeekStart())->setTime(18, 0));
+        }
 
         return $this->createForm(EventFormType::class, $event, [
             'userTimezone' => $this->aureumService->getEmployee()->getUser()->getTimezone(),
         ]);
     }
 
+    private function findEditEvent(): ?Event
+    {
+        if ($this->editEventId === null) {
+            return null;
+        }
+
+        $event = $this->eventRepository->find($this->editEventId);
+
+        if ($event === null || $event->getHotel()->getId() !== $this->aureumService->getHotel()->getId()) {
+            return null;
+        }
+
+        return $event;
+    }
     public function getWeekStart(): \DateTimeImmutable
     {
         return $this->resolveWeekStart($this->week);
@@ -139,6 +196,17 @@ class EventComponent extends AbstractController
         return EventType::cases();
     }
 
+    public function canEdit(): bool
+    {
+        return $this->aureumService->getEmployee()->getRole()->canManageEvents();
+    }
+
+    private function denyUnlessAllowed(): void
+    {
+        if (!$this->canEdit()) {
+            throw new AccessDeniedException('you cannot manage events.');
+        }
+    }
     private function resolveWeekStart(string $week): \DateTimeImmutable
     {
         if ($week !== '') {
