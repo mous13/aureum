1d952ac5af39edda6e983c354a2d5069c3d97133
Oscar Ellis <oscarx1be@gmail.com>
2026-08-15 10:23:59 +0100

    Add missing generics docblock to InventoryItemLogRepository

--- src/Core/Repository/InventoryItemLogRepository.php
+++ src/Core/Repository/InventoryItemLogRepository.php
@@ -7,6 +7,9 @@ namespace Citadel\Aureum\Core\Repository;
 use Citadel\Aureum\Core\Entity\InventoryItem;
 use Citadel\Aureum\Core\Entity\InventoryItemLog;
 
+/**
+ * @extends AbstractLogRepository<InventoryItemLog>
+ */
 class InventoryItemLogRepository extends AbstractLogRepository
 {
     public static function getEntityClass(): string
